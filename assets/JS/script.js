2b1b4f6e115e2e43adaceb0ad4b55a4c86b78e31
runBIGGINrun <114839299+runBIGGINrun@users.noreply.github.com>
2022-11-14 17:29:19 -0600

    added variables for time check and for the time array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,3 +3,13 @@ let currentDayDisplay = document.getElementById('currentDay');
 let hoursContainer = document.getElementById('hoursList');
 let thisDay = moment().format('LL');
 currentDayDisplay.textContent = thisDay;
+
+// Time prep for checking hours
+let timeIncoming = moment.utc();
+timeIncoming.local();
+let thisHour = timeIncoming.hours();
+console.log(thisHour)
+
+
+let hoursAvailable = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+console.log(hoursAvailable);
