9fb9707dd19db3254af21068820e274671682823
runBIGGINrun <114839299+runBIGGINrun@users.noreply.github.com>
2022-11-14 17:31:52 -0600

    created for-loop for writing hours

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,3 +13,14 @@ console.log(thisHour)
 
 let hoursAvailable = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
 console.log(hoursAvailable);
+
+
+function writeHours() {
+
+    for (let i = 0; i < hoursAvailable.length; i++) {
+        //  writes Elements
+        let divEl = document.createElement('div');
+        let divEl2 = document.createElement('div');
+        let textArea = document.createElement('textarea');
+        let buttonEl = document.createElement('button');
+        let iconEl = document.createElement('i');
